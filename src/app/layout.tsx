11636297d4f016cee98015577d5795f54dd94165
kinhdoanhmoc12-collab <kinhdoanhmoc12@gmail.com>
2026-07-28 11:05:49 +0700

    feat: integrate Google Tag Manager (GTM-T23ZMSWR) scripts in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "../context/LanguageContext";
 import LayoutWrapper from "../components/LayoutWrapper";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,7 +27,32 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&dl='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-T23ZMSWR');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col bg-[#fcfcfc] text-[#0f172a] font-sans">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T23ZMSWR"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <LanguageProvider>
           <LayoutWrapper>
             {children}
@@ -36,3 +62,4 @@ export default function RootLayout({
     </html>
   );
 }
+
